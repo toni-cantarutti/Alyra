d10f8b1de3eec3009760bda38c16176393bb8cdf
Toni <toni.cantarutti@gmail.com>
2024-10-13 21:02:32 +0200

    Fix: deployment scripts & cleaning

---
+++ Contracts/Sandbox/AdvTest.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.28;
+
+contract Base {
+    uint256 public interger;
+}
+
+/**
+ * @title AdvTest
+ * @dev 
+ * @custom:dev-run-script ./scripts/generic_deploy_with_ethers.ts
+ */
+contract AdvTest{
+    constructor(){
+
+    }
+}
+ 
--- scripts/generic_deploy_with_ethers.ts
+++ scripts/generic_deploy_with_ethers.ts
@@ -1,8 +1,10 @@
-import { deploy } from './ethers-lib'
+import { deploy } from './custom_ethers-lib'
 ;(async () => {
     try {
         const solFile = await remix.call('fileManager', 'getCurrentFile')
         const buildFile = "browser/"+solFile.replace(/\.sol$/, '.json').replace(/\/([^/]+)\.json$/, '/artifacts/$1.json')
+
+        console.log(solFile+" -> "+buildFile);
         const result = await deploy(
             buildFile,
             [],
